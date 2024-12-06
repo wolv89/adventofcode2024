74e6715009c4400844afed496a045d263ba1bc75
wolv89 <peter.delafosse@gmail.com>
2024-12-06 16:37:30 +1100

    Day 5 - Puzzle 2

--- day05/day05.go
+++ day05/day05.go
@@ -2,9 +2,11 @@ package day05
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -117,7 +119,129 @@ func (d AocDay5) Puzzle1(useSample bool) {
 
 }
 
+type PageRules struct {
+	rules int
+	page  uint8
+}
+
 func (d AocDay5) Puzzle2(useSample bool) {
+
+	datafile := DIR + "data.txt"
+	if useSample {
+		datafile = DIR + "sample.txt"
+	}
+
+	f, err := os.Open(datafile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var (
+		rawNums               []string
+		line                  string
+		n, i, total, mid, rls int
+		num, n1, n2, page     uint8
+		ok, valid             bool
+	)
+
+	allRules := make(map[uint8][]uint8)
+
+	pages := make(map[uint8]struct{})
+	rules := make(map[uint8][]uint8)
+
+	seen := make(map[uint8]struct{})
+
+	readingRules := true
+
+	for scanner.Scan() {
+
+		line = scanner.Text()
+
+		if len(line) == 0 {
+			readingRules = false
+			continue
+		}
+
+		if readingRules {
+
+			n1, n2 = RuleToInts(line)
+			if n1 == 0 || n2 == 0 {
+				fmt.Println("Problem parsing numbers from: ", line)
+				continue
+			}
+
+			allRules[n1] = append(allRules[n1], n2)
+
+		} else {
+
+			rawNums = strings.Split(line, ",")
+			clear(pages)
+			clear(rules)
+			clear(seen)
+			list := make([]uint8, 0)
+			valid = true
+
+			for n = 0; n < len(rawNums); n++ {
+				num = NumToInt(rawNums[n])
+				pages[num] = struct{}{}
+				list = append(list, num)
+			}
+
+			for page = range pages {
+				for _, num = range allRules[page] {
+					if _, ok = pages[num]; ok {
+						rules[page] = append(rules[page], num)
+					}
+				}
+			}
+
+			n = len(list) - 1
+
+		review:
+			for i = n; i >= 0; i-- {
+				for _, num = range rules[list[i]] {
+					if _, ok = seen[num]; !ok {
+						valid = false
+						break review
+					}
+				}
+				seen[list[i]] = struct{}{}
+			}
+
+			if valid {
+				fmt.Println("[ ] | Already valid, skipping")
+				continue
+			}
+
+			// Sorting pages by the number of rules
+			// Is this "cheating" ... ?
+			prs := make([]PageRules, 0, len(rules))
+			for _, page = range list {
+				rls = 0
+				if _, ok = rules[page]; ok {
+					rls = len(rules[page])
+				}
+				prs = append(prs, PageRules{rls, page})
+			}
+
+			slices.SortFunc(prs, func(a, b PageRules) int {
+				return cmp.Compare(b.rules, a.rules)
+			})
+
+			mid = int(prs[n/2].page)
+			total += mid
+			fmt.Println("[x] |", mid, "|", total, " - ", prs)
+
+		}
+
+	}
+
+	fmt.Println("")
+	fmt.Println("Total: ", total)
 
 }
 
