b0b0174b3d51cb242d11f1a35cfa313f792678e6
wolv89 <peter.delafosse@gmail.com>
2024-12-09 18:40:19 +1100

    Day 7 - Puzzle 2

--- day07/day07.go
+++ day07/day07.go
@@ -13,11 +13,6 @@ type AocDay7 struct{}
 
 const DIR = "day07/"
 
-var OPS = [2]byte{
-	'*',
-	'+',
-}
-
 func (d AocDay7) Puzzle1(useSample bool) {
 
 	datafile := DIR + "data.txt"
@@ -39,7 +34,7 @@ func (d AocDay7) Puzzle1(useSample bool) {
 		nums               []int64
 		line               string
 		target, num, total int64
-		p, lim             int
+		p                  int
 	)
 
 outer:
@@ -73,25 +68,11 @@ outer:
 		}
 
 		if Calc(nums, nums[0], target, 1) {
-			fmt.Println(lim, " | VALID - ", target)
 			total += target
-		} else {
-			fmt.Println(lim, " | Invalid - ", target)
 		}
-
-		fmt.Println("--------------")
-
-		lim++
-		// if lim > 10 {
-		// 	break
-		// }
 
 	}
 
-	// fmt.Println("")
-	// for v, vld := range valids {
-	// 	fmt.Println(v, " > ", vld)
-	// }
 	fmt.Println("")
 	fmt.Println("Total: ", total)
 
@@ -118,5 +99,92 @@ func Calc(nums []int64, sum, target int64, n int) bool {
 }
 
 func (d AocDay7) Puzzle2(useSample bool) {
+
+	datafile := DIR + "data.txt"
+	if useSample {
+		datafile = DIR + "sample.txt"
+	}
+
+	f, err := os.Open(datafile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var (
+		parts              []string
+		nums               []int64
+		line               string
+		target, num, total int64
+		p                  int
+	)
+
+outer:
+	for scanner.Scan() {
+
+		line = scanner.Text()
+
+		parts = strings.Split(line, " ")
+
+		nums = make([]int64, 0, len(parts)-1)
+
+		for p = 0; p < len(parts); p++ {
+
+			if p == 0 {
+				parts[p] = strings.TrimSuffix(parts[p], ":")
+			}
+
+			num, err = strconv.ParseInt(parts[p], 10, 64)
+
+			if err != nil {
+				fmt.Println("ERR: Unable to parse ", parts[p], " | ", err)
+				continue outer
+			}
+
+			if p == 0 {
+				target = num
+			} else {
+				nums = append(nums, num)
+			}
+
+		}
+
+		if CalcTwo(nums, nums[0], target, 1) {
+			total += target
+		}
+
+	}
+
+	fmt.Println("")
+	fmt.Println("Total: ", total)
+
+}
+
+func CalcTwo(nums []int64, sum, target int64, n int) bool {
+
+	if n == len(nums) && sum == target {
+		return true
+	} else if sum > target || n == len(nums) {
+		return false
+	}
+
+	return CalcTwo(nums, sum*nums[n], target, n+1) || CalcTwo(nums, IntConcat(sum, nums[n]), target, n+1) || CalcTwo(nums, sum+nums[n], target, n+1)
+
+}
+
+func IntConcat(n1, n2 int64) int64 {
+
+	t := int64(1)
+
+	// Was only doing a < check initially
+	// Which meant I was off by a lazy 140 TRILLION in the final answer...
+	for t <= n2 {
+		t *= 10
+	}
+
+	return n1*t + n2
 
 }
