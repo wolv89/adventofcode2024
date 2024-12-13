3b97fec9059c172b4d1246da86f065baad17464b
wolv89 <peter.delafosse@gmail.com>
2024-12-13 22:50:15 +1100

    Day 11 - Puzzle 1

--- day11/day11.go
+++ day11/day11.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -18,6 +19,10 @@ func (d AocDay11) Puzzle1(useSample int) {
 	datafile := DIR + "data.txt"
 	if useSample == 1 {
 		datafile = DIR + "sample.txt"
+	} else if useSample == 2 {
+		datafile = DIR + "sample2.txt"
+	} else if useSample == 3 {
+		datafile = DIR + "sample3.txt"
 	}
 
 	f, err := os.Open(datafile)
@@ -33,9 +38,15 @@ func (d AocDay11) Puzzle1(useSample int) {
 	start := strings.Split(line, " ")
 	stones := make([]int64, 0, len(start))
 
-	for _, st := range start {
+	var (
+		st        string
+		stone, mp int64
+		b, dg     int
+	)
 
-		stone, err := strconv.ParseInt(st, 10, 64)
+	for _, st = range start {
+
+		stone, err = strconv.ParseInt(st, 10, 64)
 		if err != nil {
 			fmt.Println("Unable to convert... ", st, " | ", err)
 			continue
@@ -46,8 +57,58 @@ func (d AocDay11) Puzzle1(useSample int) {
 	}
 
 	fmt.Println(stones)
+	fmt.Println("---")
+
+	const BLINKS = 50
+
+	for b = 0; b < BLINKS; b++ {
+
+		next := make([]int64, 0, len(stones))
+
+		for _, stone = range stones {
+
+			if stone == 0 {
+				next = append(next, 1)
+				continue
+			}
+
+			dg = countDigits(stone)
+
+			if dg%2 == 0 {
+				mp = int64(math.Pow10(dg / 2))
+				next = append(next, stone/mp)
+				next = append(next, stone%mp)
+				continue
+			}
+
+			next = append(next, stone*2024)
+
+		}
+
+		stones = next
+
+		if BLINKS <= 6 {
+			fmt.Println(stones)
+		} else {
+			fmt.Println(b+1, ": ", len(stones))
+		}
+
+	}
 
 	fmt.Println("")
+
+}
+
+func countDigits(num int64) int {
+
+	d, m := 0, int64(1)
+
+	for m <= num {
+		m *= 10
+		d++
+	}
+
+	return d
 
 }
 
