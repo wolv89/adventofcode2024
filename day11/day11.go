3320087daf7254460eebe5d72f78b5899129f378
wolv89 <peter.delafosse@gmail.com>
2024-12-14 18:47:32 +1100

    Day 11 - Puzzle 2 Failed brute force attempt with goroutines

--- day11/day11.go
+++ day11/day11.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type AocDay11 struct{}
@@ -23,6 +24,8 @@ func (d AocDay11) Puzzle1(useSample int) {
 		datafile = DIR + "sample2.txt"
 	} else if useSample == 3 {
 		datafile = DIR + "sample3.txt"
+	} else if useSample == 4 {
+		datafile = DIR + "sample4.txt"
 	}
 
 	f, err := os.Open(datafile)
@@ -59,7 +62,7 @@ func (d AocDay11) Puzzle1(useSample int) {
 	fmt.Println(stones)
 	fmt.Println("---")
 
-	const BLINKS = 50
+	const BLINKS = 10
 
 	for b = 0; b < BLINKS; b++ {
 
@@ -87,8 +90,8 @@ func (d AocDay11) Puzzle1(useSample int) {
 
 		stones = next
 
-		if BLINKS <= 6 {
-			fmt.Println(stones)
+		if BLINKS <= 10 {
+			fmt.Println(len(stones), " ", stones)
 		} else {
 			fmt.Println(b+1, ": ", len(stones))
 		}
@@ -112,6 +115,131 @@ func countDigits(num int64) int {
 
 }
 
+type Counter struct {
+	count int64
+	mu    sync.RWMutex
+}
+
 func (d AocDay11) Puzzle2(useSample int) {
+
+	datafile := DIR + "data.txt"
+	if useSample == 1 {
+		datafile = DIR + "sample.txt"
+	} else if useSample == 2 {
+		datafile = DIR + "sample2.txt"
+	} else if useSample == 3 {
+		datafile = DIR + "sample3.txt"
+	}
+
+	f, err := os.Open(datafile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	line := scanner.Text()
+
+	start := strings.Split(line, " ")
+	stones := make([]int64, 0, len(start))
+
+	var (
+		st    string
+		stone int64
+		cnt   Counter
+		wg    sync.WaitGroup
+	)
+
+	for _, st = range start {
+
+		stone, err = strconv.ParseInt(st, 10, 64)
+		if err != nil {
+			fmt.Println("Unable to convert... ", st, " | ", err)
+			continue
+		}
+
+		stones = append(stones, stone)
+
+	}
+
+	fmt.Println(stones)
+	fmt.Println("---")
+
+	// Should probably make this a CLI flag...
+	const BLINKS = 45
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		countStones(stones, 0, BLINKS, &cnt)
+	}()
+
+	wg.Wait()
+
+	fmt.Println("")
+	fmt.Println("Count: ", cnt.count)
+
+}
+
+func countStones(stones []int64, blink, BLINKS int, cnt *Counter) {
+
+	next := make([]int64, 0, len(stones))
+
+	// fmt.Println(stones, " | ", blink, "/", BLINKS)
+
+	for _, stone := range stones {
+
+		if stone == 0 {
+			next = append(next, 1)
+			continue
+		}
+
+		dg := countDigits(stone)
+
+		if dg%2 == 0 {
+			mp := int64(math.Pow10(dg / 2))
+			next = append(next, stone/mp)
+			next = append(next, stone%mp)
+			continue
+		}
+
+		next = append(next, stone*2024)
+
+	}
+
+	blink++
+
+	if blink == BLINKS {
+
+		cnt.mu.Lock()
+		cnt.count += int64(len(next))
+		cnt.mu.Unlock()
+
+		return
+
+	}
+
+	if len(next) <= 10 {
+		countStones(next, blink, BLINKS, cnt)
+	} else {
+		mid := len(next) / 2
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			countStones(next[:mid], blink, BLINKS, cnt)
+		}()
+
+		go func() {
+			defer wg.Done()
+			countStones(next[mid:], blink, BLINKS, cnt)
+		}()
+
+		wg.Wait()
+	}
 
 }
